1e693edfaa23bbdfe430d52954eee9b64e0a7cac
David Robb <66834033+drobb2020@users.noreply.github.com>
2024-01-31 20:39:17 -0500

    User profile

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,14 +1,93 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-from .models import Category, Customer, Order, Product
+from .models import Category, Customer, Order, Product, Profile
 
 
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "date_modified",
+        "phone",
+        "address1",
+        "address2",
+        "city",
+        "state",
+        "zipcode",
+        "country",
+    )
+    list_filter = ("user", "date_modified")
+
+
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {"slug": ('name',)}
+    list_display = ("id", "name", "slug")
+    search_fields = ("name", "slug")
+    prepopulated_fields = {"slug": ["name"]}
 
 
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Product)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "phone",
+        "email",
+        "password",
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "price",
+        "category",
+        "description",
+        "image",
+        "is_sale",
+        "sale_price",
+    )
+    list_filter = ("category", "is_sale")
+    search_fields = ("name",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "product",
+        "customer",
+        "quantity",
+        "address",
+        "phone",
+        "date",
+        "status",
+    )
+    list_filter = ("product", "customer", "date", "status")
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+    )
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+
+
+admin.site.register(User, UserAdmin)
--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,7 +1,33 @@
 import datetime
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 from django.urls import reverse
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    zipcode = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
 
 
 class Category(models.Model):
